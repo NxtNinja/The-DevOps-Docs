182e367d1d4bc79767bd6210458326293966ca61
NxtNinja <136341993+NxtNinja@users.noreply.github.com>
2024-05-18 16:44:22 +0530

    virtualization added

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import "@/styles/globals.css";
+
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({ subsets: ['latin'], weight: '400' })
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={raleway.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+    logo: <strong>The DevOps Documentation</strong>,
+    footer: {
+        text: `Copyright Priyangsu Banik ${new Date().getFullYear()}`
+    },
+    project: {
+        link: 'https://github.com/shuding/nextra'
+    }
+    // ... other theme options
+}
